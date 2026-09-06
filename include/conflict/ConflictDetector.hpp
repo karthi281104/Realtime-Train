b40f3bf28de06ef45d7f0b08d48fb466167d7a76
Karthikeyan A <karthikeyana281104@gmail.com>
2026-09-06 12:47:28 +0530

    Add Module 9 conflict detector interface

---
+++ include/conflict/ConflictDetector.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "common/Types.hpp"
+#include "conflict/Conflict.hpp"
+#include "prediction/FutureState.hpp"
+
+#include <vector>
+
+namespace tcas::conflict
+{
+
+class ConflictDetector
+{
+public:
+    explicit ConflictDetector(ConflictDetectionConfig config = {});
+
+    [[nodiscard]]
+    std::vector<Conflict> detect(
+        TrainId trainA,
+        const std::vector<prediction::FutureState>& trajectoryA,
+        TrainId trainB,
+        const std::vector<prediction::FutureState>& trajectoryB
+    ) const;
+
+private:
+    ConflictDetectionConfig config_;
+
+    [[nodiscard]]
+    ConflictType classifySameTrack(
+        const prediction::FutureState& a,
+        const prediction::FutureState& b
+    ) const noexcept;
+
+    [[nodiscard]]
+    bool hasTemporalConflict(
+        const prediction::FutureState& a0,
+        const prediction::FutureState& a1,
+        const prediction::FutureState& b0,
+        const prediction::FutureState& b1,
+        DistanceMeters& minimumSeparation,
+        TimeSeconds& firstTime,
+        TimeSeconds& lastTime
+    ) const noexcept;
+};
+
+} // namespace tcas::conflict
